fd1147415ff1bc8b82d5f64ca394f743adf4011e
neneu <samuel_silva_ext_1@carrefour.com>
2022-06-23 17:21:04 -0300

    Framework flash Code

---
+++ src/test/java/teste/teste.java
@@ -0,0 +1,51 @@
+package teste;
+
+import java.util.Scanner;
+
+public class teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		String pais = scanner.next();
+		
+		
+		switch (pais) {
+		case "Brasil":
+			System.out.println("bem vindo ao brasil");
+			break;
+
+		
+			
+		}
+		
+
+		
+
+			
+			
+	}
+}
+
+
+
+/*public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+	Scanner scanner = new Scanner(System.in);
+	
+	
+	System.out.println("Digite seu sabor :");
+	String sabor = scanner.toString();
+	switch (sabor) {
+	case "mussarela":
+		System.out.println("sabor mussarela");
+		break;
+
+	default:
+		break;
+	}
+}
+
+}*/
